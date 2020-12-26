8161f8bab1093f1d35a656ca51160f205eba72bc
kochtaik <kochtaik@gmail.com>
2020-12-26 13:36:43 +0300

    start using express.js

--- node-js/server.js
+++ node-js/server.js
@@ -1,6 +1,20 @@
-const http = require('http');
-const routes = require('./routes');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-const server = http.createServer(routes);
+app.use(bodyParser.urlencoded({extended: false}));
 
-server.listen(3000);
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="product-type"><button type="submit">Send</button></form>');
+});
+
+app.post('/product', (req, res, next) => {
+  res.redirect('/');
+  console.log(req.body);
+});
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Welcome to the main page!</h1>');
+});
+
+app.listen(3000);
