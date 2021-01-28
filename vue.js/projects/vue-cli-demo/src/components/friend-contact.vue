cdd7351a0c95b5552756f424042282edd3239347
kochtaik <kochtaik@gmail.com>
2021-01-28 17:03:54 +0300

    move to an advanced project setup

---
+++ vue.js/projects/vue-cli-demo/src/components/friend-contact.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleVisibility">{{ detailsAreVisible ? 'Hide' : 'Show'}} details</button>
+    <ul v-if="detailsAreVisible">
+      <li>{{ friend.phone }}</li>
+      <li>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: 'Serge',
+        name: 'Serge',
+        phone: '0123-45-67',
+        email: 'serge@localhost.com',
+      },
+    };
+  },
+  methods: {
+    toggleVisibility() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+}
+</script>
